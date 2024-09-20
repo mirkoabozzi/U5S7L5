1cd5629d2007858a40d8a422f927691ff0c2c14a
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-09-20 09:19:17 +0200

    Initial commit

---
+++ src/test/java/mirkoabozzi/U5S7L5/U5S7L5ApplicationTests.java
@@ -0,0 +1,13 @@
+package mirkoabozzi.U5S7L5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5S7L5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
